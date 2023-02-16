62f8558b972fed4bf5313c34aea9551dbac37709
Pharos19 <Pharos199@gmail.com>
2023-02-16 10:44:16 -0700

    working with api and js

--- Weather-Dashboard/Assets/js/script.js
+++ Weather-Dashboard/Assets/js/script.js
@@ -5,3 +5,38 @@ var cityCardEl = document.querySelector("#city-card");
 
 // hero variables
 var searchedCity = document.querySelector("#searched-city");
+var cityTemp = document.querySelector("#city-temp");
+var cityWind = document.querySelector("#city-wind");
+var cityHumidity = document.querySelector("#city-humidity");
+var cityUv = document.querySelector("#city-uv");
+
+var cityArr = [];
+
+// API request
+var getCityWeather = function (city) {
+    var apiUr1 =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&units=imperial&appid=df2e9a434e3478cdfaae84165503d8aa";
+
+    fetch(apiUr1)
+     .then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                // get lat and lon for getForecast
+                var lat = data.coord.lat;
+                var lon = data.coord.lon;
+                getForecast(lat,lon);
+                // input city name
+                searchedCity.textContent = city;
+            });
+        } else {
+            alert("Could not find city, please try again");
+        }
+     })
+     .catch(function (error) {
+        alert("Unable to connect to OpenWeather");
+     });
+};
+
+var getForecast
