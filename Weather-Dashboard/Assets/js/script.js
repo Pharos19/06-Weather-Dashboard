a6d190c9479917fd8063bba45e7a31fbd9742967
Pharos19 <Pharos199@gmail.com>
2023-02-16 17:33:43 -0700

    working on readME

--- Weather-Dashboard/Assets/js/script.js
+++ Weather-Dashboard/Assets/js/script.js
@@ -1,4 +1,4 @@
-var APIkey = "df2e9a434e3478cdfaae84165503d8aa"
+// APIkey = "df2e9a434e3478cdfaae84165503d8aa"
 var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector(".city-search");
 var cityCardEl = document.querySelector("#city-card");
